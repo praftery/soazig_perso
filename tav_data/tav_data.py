d28522476abe249f2ce0ad90d6f801a297275648
soazig <soazig.kaam@berkeley.edu>
2015-09-19 21:00:22 -0700

    for zones S5-09 PIER report

--- tav_data/tav_data.py
+++ tav_data/tav_data.py
@@ -32,38 +32,37 @@ path_list = [
              'AIR_VOLUME'
              ] 
 source_tav = 'Sutardja Dai Hall TAV'
-path_and = 'tav_with_limiter'
+path_and = 'tav_whole_bldg'
 path_list_tav = [
-#                'tav_active',
-#                'average_airflow_in_cycle',
+                'tav_active',
                 '/cycle'
                 ]
-
-restrict = " Metadata/SourceName = '%s' and Path ~ 'S7-16' and ("\
-             %(source)\
+zone_name = 'S5-09'
+restrict = " Metadata/SourceName = '%s' and Path ~ '%s' and ("\
+             %(source, zone_name)\
              + ' or '.join(["Path ~ '%s'"] * len(path_list)) \
              %tuple(path_list) + ")"
 
-restrict2 = " Metadata/SourceName = '%s' and Path ~ '%s' and Path ~ 'S7-16' and ("\
-             %(source_tav, path_and) \
+restrict2 = " Metadata/SourceName = '%s' and Path ~ '%s' and Path ~ '%s' and ("\
+             %(source_tav, zone_name, path_and) \
              + ' or '.join(["Path ~ '%s'"] * len(path_list_tav)) \
              %tuple(path_list_tav) + ")"
 
-restrictall = " (" + restrict + ") or (" + restrict2 + ")"
-tags = c.tags(restrict2)
+restrictall = "(" + restrict + ") or (" + restrict2 + ")"
+tags = c.tags(restrictall)
 
 print "Tags len: ", len(tags)
-startDate = "08/28/2015"
-endDate = "08/29/2015"
+startDate = "09/07/2015"
+endDate = "09/09/2015"
 #start = dtutil.dt2ts(dtutil.strptime_tz("08-28-2015", "%m-%d-%Y"))
 #end   = dtutil.dt2ts(dtutil.strptime_tz("08-29-2015", "%m-%d-%Y"))
 #data = c.data(restrictall, start, end)
 #pp.pprint(data)
 #pdb.set_trace()
 
-name = 'S7-16_tav_cycle.csv' 
+name = zone_name + '_tav_cycle.csv' 
 dt_format = '%Y-%m-%d %H:%M:%S'
-query_data = 'select data in ("' + startDate + '" , "' + endDate + '") where' + restrict2
+query_data = 'select data in ("' + startDate + '" , "' + endDate + '") where' + restrictall
 data = c.query(query_data)
 #data = c.data(restrictall, start, end, limiit=1000)
 
@@ -80,10 +79,9 @@ for i in range(N):
   if d.any():
     tag_path = [tag['Path'] for tag in tags if tag['uuid'] == u][0]
     print tag_path
-    for p in path_list_tav:
+    for p in path_list_tav + path_list:
       if p in tag_path:
         df[p] = d[:,1]
       print " Dwnl data for " + p
-
 
 df.to_csv(name)

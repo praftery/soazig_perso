277c378530685349b4d0e225231dcbb081f3dc82
soazig <soazig.kaam@berkeley.edu>
2016-01-13 10:11:10 -0800

    modif name + looping

--- tav_data/scripts/tav_data.py
+++ tav_data/scripts/tav_data.py
@@ -8,27 +8,35 @@ from smap.contrib import dtutil
 from smap.util import find
 from datetime import timedelta, date
 
+import sys, os, pdb
 import numpy as np
 import pandas as pd
-import pdb
 import csv
 import shutil
 import time
 import pprint as pp
 import datetime
 
+sys.path.append(os.path.join(os.path.dirname(__file__), "../functions/"))
+import tav_graphs_functions
+
 c = SmapClient(base='http://new.openbms.org/backend',\
                key=['XuETaff882hB6li0dP3XWdiGYJ9SSsFGj0N8'])
 t = time.time()
 source = 'Sutardja Dai Hall BACnet'
+#path_list = [
+#             'HEAT.COOL',
+#             'VLV_POS',
+#             'CTL_STPT',
+#             'ROOM_TEMP',
+#             'AI_3',
+#             'CTL_FLOW_MIN',
+#             'DMPR_POS',
+#             'AIR_VOLUME'
+#             ] 
 path_list = [
-             'HEAT.COOL',
-             'VLV_POS',
-             'CTL_STPT',
-             'ROOM_TEMP',
-             'AI_3',
+             'CTL_FLOW_MAX',
              'CTL_FLOW_MIN',
-             'DMPR_POS',
              'AIR_VOLUME'
              ] 
 source_tav = 'Sutardja Dai Hall TAV'
@@ -37,51 +45,62 @@ path_list_tav = [
                 'tav_active',
                 '/cycle'
                 ]
-zone_name = 'S5-03'
-restrict = " Metadata/SourceName = '%s' and Path ~ '%s' and ("\
-             %(source, zone_name)\
-             + ' or '.join(["Path ~ '%s'"] * len(path_list)) \
-             %tuple(path_list) + ")"
-
-restrict2 = " Metadata/SourceName = '%s' and Path ~ '%s' and Path ~ '%s' and ("\
-             %(source_tav, zone_name, path_and) \
-             + ' or '.join(["Path ~ '%s'"] * len(path_list_tav)) \
-             %tuple(path_list_tav) + ")"
-
-restrictall = "(" + restrict + ") or (" + restrict2 + ")"
-tags = c.tags(restrictall)
-
-print "Tags len: ", len(tags)
-startDate = "10/01/2015 14:00"
-endDate = "10/01/2015 21:01"
-#start = dtutil.dt2ts(dtutil.strptime_tz("08-28-2015", "%m-%d-%Y"))
-#end   = dtutil.dt2ts(dtutil.strptime_tz("08-29-2015", "%m-%d-%Y"))
-#data = c.data(restrictall, start, end)
-#pp.pprint(data)
-#pdb.set_trace()
-
-name = zone_name + '_tav_cycle.csv' 
-dt_format = '%Y-%m-%d %H:%M:%S'
-query_data = 'select data in ("' + startDate + '" , "' + endDate + '") where' + restrictall
-data = c.query(query_data)
-#data = c.data(restrictall, start, end, limiit=1000)
-
-N=len(data)
-df = pd.DataFrame()
-d = np.array(data[0]['Readings'])
-df['timestamp'] = d[:,0]
-dt_format = '%Y-%m-%d %H:%M:%S'
-df['datetime'] = [datetime.datetime.fromtimestamp(x/1000).strftime(dt_format)
-                  for x in d[:,0]]
-for i in range(N):
-  u = data[i]['uuid']
-  d = np.array(data[i]['Readings'])
-  if d.any():
-    tag_path = [tag['Path'] for tag in tags if tag['uuid'] == u][0]
-    print tag_path
-    for p in path_list_tav + path_list:
-      if p in tag_path:
-        df[p] = d[:,1]
-      print " Dwnl data for " + p
-
-df.to_csv(name)
+zone_list = ['S4-18', 'S4-03', 'S4-05', 'S4-09']
+for zone_name in zone_list:
+  dffull = pd.DataFrame() 
+  #for path_list in points:
+  if True:
+    restrict = " Metadata/SourceName = '%s' and Path ~ '%s' and ("\
+                 %(source, zone_name)\
+                 + ' or '.join(["Path ~ '%s'"] * len(path_list)) \
+                 %tuple(path_list) + ")"
+    
+    restrict2 = " Metadata/SourceName = '%s' and Path ~ '%s' and Path ~ '%s' and ("\
+                 %(source_tav, zone_name, path_and) \
+                 + ' or '.join(["Path ~ '%s'"] * len(path_list_tav)) \
+                 %tuple(path_list_tav) + ")"
+    
+    restrictall = "(" + restrict + ") or (" + restrict2 + ")"
+    tags = c.tags(restrict)
+    
+    start = date(2015,11,9)
+    delta = datetime.timedelta(days=2)
+    end = start + delta
+    endDateF = date(2016, 12, 26)
+    #data = c.data(restrictall, start, end)
+    while end <= endDateF: 
+      startDate = start.strftime("%m/%d/%Y %H:%M")
+      endDate = end.strftime("%m/%d/%Y %H:%M")
+      #Start = dtutil.dt2ts(dtutil.strptime_tz(start.strftime("%m-%d-%Y"), "%m-%d-%Y"))
+      #End = dtutil.dt2ts(dtutil.strptime_tz(end.strftime("%m-%d-%Y"), "%m-%d-%Y"))
+      print "Start date: ", startDate
+      print "End date: ", endDate
+      name = '../csv_output/' + zone_name + '_airflow' +  start.strftime("%Y%m%d")+'.csv' 
+      dt_format = '%Y-%m-%d %H:%M:%S'
+      query_data = 'select data in ("' + startDate + '" , "' + endDate + '") where' + restrict
+      data = c.query(query_data)
+      #data = c.data(restrict, Start, End, limit=1000000000)
+      N=len(data)
+      df = pd.DataFrame()
+      d = np.array(data[0]['Readings'])
+      df['timestamp'] = d[:,0]
+      dt_format = '%Y-%m-%d %H:%M:%S'
+      df['datetime'] = [datetime.datetime.fromtimestamp(x/1000).strftime(dt_format)
+                        for x in d[:,0]]
+      for i in range(N):
+        u = data[i]['uuid']
+        d = np.array(data[i]['Readings'])
+        if d.any():
+          tag_path = [tag['Path'] for tag in tags if tag['uuid'] == u][0]
+          print tag_path
+          for p in path_list:
+            if p in tag_path:
+              h = '_'.join(tag_path.split('/')[-2:])
+              df[h] = d[:,1]
+              print " Dwnl data for " + h
+      dffull.append(df)
+      start = end
+      end += delta
+    df_aug = tav_graphs_functions.data_ratios(dffull, path_list) 
+    dffull.to_csv(name)
+    pdb.set_trace()
--- tav_data/scripts/tav_energy_data.py
+++ tav_data/scripts/tav_energy_data.py
@@ -22,8 +22,25 @@ c = SmapClient(base='http://new.openbms.org/backend',\
 t = time.time()
 source_energy = 'Sutardja Dai Hall Energy Data'
 path_and = 'energy_data/variable_elec_cost/'
+#path_list_energy = [
+#                'total_cost',
+#                'AH2_panel_fan_power',
+#                'AH2_panel_fan_power_cost',
+#                'chilled_water_AH2',
+#                'chilled_water_AH2_cost',
+#                'hot_water_AH2',
+#                'hot_water_AH2_cost'
+#                ]
 path_list_energy = [
-                'total_cost'
+                'total_zone_load_AH2',
+                'instantaneous_zone_load_S4-03'
+                'instantaneous_zone_load_S4-05'
+                'instantaneous_zone_load_S4-02'
+                'instantaneous_zone_load_S4-18'
+                'hot_water_S4-03'
+                'hot_water_S4-05'
+                'hot_water_S4-02'
+                'hot_water_S4-18'
                 ]
 
 restrict = " Metadata/SourceName = '%s' and Path ~ '%s' and ("\
@@ -32,38 +49,54 @@ restrict = " Metadata/SourceName = '%s' and Path ~ '%s' and ("\
              %tuple(path_list_energy) + ")"
 
 tags = c.tags(restrict)
+frames = []
+startDateF = date(2015, 11, 9)
+startDate = startDateF
+endDateF = date(2015, 12, 26)
+delta = datetime.timedelta(days=2)
+endDate = startDate + delta
+while endDate <= endDateF:
+  print "Start date: ", startDate
+  print "End date: ", endDate
 
-print "Tags len: ", len(tags)
-startDate = "12/13/2015 11:00"
-endDate = "12/13/2015 13:00"
-#start = dtutil.dt2ts(dtutil.strptime_tz("08-28-2015", "%m-%d-%Y"))
-#end   = dtutil.dt2ts(dtutil.strptime_tz("08-29-2015", "%m-%d-%Y"))
-#data = c.data(restrictall, start, end)
-#pp.pprint(data)
-#pdb.set_trace()
-
-name = 'energy_data1213_test.csv' 
-dt_format = '%Y-%m-%d %H:%M:%S'
-query_data = 'select data in ("' + startDate + '" , "' + endDate + '") where' + restrict
-data = c.query(query_data)
-#data = c.data(restrictall, start, end, limit=1000)
-
-N=len(data)
-df = pd.DataFrame()
-d = np.array(data[0]['Readings'])
-df['timestamp'] = d[:,0]
-dt_format = '%Y-%m-%d %H:%M:%S'
-df['datetime'] = [datetime.datetime.fromtimestamp(x/1000).strftime(dt_format)
-                  for x in d[:,0]]
-for i in range(N):
-  u = data[i]['uuid']
-  d = np.array(data[i]['Readings'])
-  if d.any():
-    tag_path = [tag['Path'] for tag in tags if tag['uuid'] == u][0]
-    print tag_path
-    for p in path_list_energy:
-      if p in tag_path:
-        df[p] = d[:,1]
-      print " Dwnl data for " + p
-
-df.to_csv(name)
+  #startDate = "11/09/2015 00:00"
+  #endDate = "12/26/2015 00:00"
+  #start = dtutil.dt2ts(dtutil.strptime_tz("08-28-2015", "%m-%d-%Y"))
+  #end   = dtutil.dt2ts(dtutil.strptime_tz("08-29-2015", "%m-%d-%Y"))
+  #data = c.data(restrictall, start, end)
+  #pp.pprint(data)
+  #pdb.set_trace()
+  
+  dt_format = '%Y-%m-%d %H:%M:%S'
+  query_data = 'apply window(first, field=\"minute\", width=3) to data in ("' + startDate.strftime("%m/%d/%Y") + '" , "' + endDate.strftime("%m/%d/%Y") + '") where' + restrict
+  data = c.query(query_data)
+  #data = c.data(restrictall, start, end, limit=1000)
+  
+  N=len(data)
+  df = pd.DataFrame()
+  d = np.array(data[0]['Readings'])
+  df['timestamp'] = d[:,0]
+  dt_format = '%Y-%m-%d %H:%M:%S'
+  df['datetime'] = [datetime.datetime.fromtimestamp(x/1000).strftime(dt_format)
+                    for x in d[:,0]]
+  for i in range(N):
+    u = data[i]['uuid']
+    d = np.array(data[i]['Readings'])
+    if d.any():
+      tag_path = [tag['Path'] for tag in tags if tag['uuid'] == u][0]
+      try:
+        for p in path_list_energy:
+          if p in tag_path:
+            df[p] = d[:,1]
+            print " Dwnl data for " + p
+      except:
+        print "Could not dwn data for : ", p
+  df.to_csv('../csv_output/Energy/Energy2_TAV_' + startDate.strftime("%Y%m%d")\
+    + '-' + endDate.strftime("%Y%m%d") + '.csv')
+  frames.append(df)
+  startDate = endDate
+  endDate += delta
+  dffull = pd.concat(frames)
+name = '../csv_output/Energy/All_energy2_data_%s.csv' %(startDate.strftime("%Y%m%d"))
+dffull.to_csv(name)
+pdb.set_trace()

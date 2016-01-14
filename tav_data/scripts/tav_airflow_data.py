b42864269af70edb2ab0ea338a7a994c78a365f6
soazig <soazig.kaam@berkeley.edu>
2016-01-14 07:40:46 -0800

    remove zones from S1

--- tav_data/scripts/tav_airflow_data.py
+++ tav_data/scripts/tav_airflow_data.py
@@ -28,9 +28,9 @@ source = 'Sutardja Dai Hall BACnet'
 points = ['CTL_FLOW_MAX', 'CTL_FLOW_MIN', 'AIR_VOLUME'] 
 #zones_names = ['S4-18', 'S4-03', 'S4-05', 'S4-09']
 full_frames = []
-#for f in [str(f) for f in [3,4,5,6,7,2,1]]:
-not_zones = ['S1-21', 'S6-21', 'S7-17', 'S7-18','S7-19', 'S7-20']
-for f in [str(f) for f in [6,7,2,1]]:
+not_zones = ['S1-11', 'S1-12', 'S1-21', 'S6-21', 'S7-17', 'S7-18','S7-19', 'S7-20', 'S7-21']
+for f in [str(f) for f in [3,4,5,6,7,2,1]]:
+#for f in [str(f) for f in [1]]:
   floor_frames = []
   print "\nStarted process for Floor: ", f
   zones_names = ['S%s-%s' %(f, str(z).zfill(2)) for z in range(1,22)]
@@ -39,11 +39,11 @@ for f in [str(f) for f in [6,7,2,1]]:
   for zone_name in zones_names:
     print "\nStarted process for zone: ", zone_name
     zone_frames = []
-    startF = date(2015,11,9)
+    startF = date(2015,10,15)
     start = startF
     delta = datetime.timedelta(days=3)
     end = start + delta
-    endF = date(2015, 12, 26)
+    endF = date(2015, 11, 10)
     while start != end: 
       startDate = start.strftime("%m/%d/%Y %H:%M")
       endDate = end.strftime("%m/%d/%Y %H:%M")
@@ -64,25 +64,25 @@ for f in [str(f) for f in [6,7,2,1]]:
   #pdb.set_trace()
   df_floor = reduce(lambda x,y: \
              pd.merge(x, y, on=['timestamp','datetime']),floor_frames)
-  floor_path = '../csv_output/Floor/' + 'Floor%s_airflow' %(str(f).zfill(2))  \
+  floor_path = '../csv_output/Floor2014/' + 'Floor%s_airflow' %(str(f).zfill(2))  \
       + '%s-%s'%(startF.strftime("%Y%m%d"), endF.strftime("%Y%m%d"))\
       + '.csv' 
   df_floor.to_csv(floor_path)
   df_aug = tav_graphs_functions.data_ratios(df_floor, points) 
-  floor_ratio_path = '../csv_output/Floor/' + 'Floor%s_ratios' %(str(f).zfill(2))  \
+  floor_ratio_path = '../csv_output/Floor2014/' + 'Floor%s_ratios' %(str(f).zfill(2))  \
       + '%s-%s'%(startF.strftime("%Y%m%d"), endF.strftime("%Y%m%d"))\
       + '.csv' 
   df_aug.to_csv(floor_ratio_path)
   full_frames.append(df_floor)
-#pdb.set_trace()
+pdb.set_trace()
 df_full = reduce(lambda x,y: \
              pd.merge(x, y, on=['timestamp','datetime']),full_frames)
-full_path = '../csv_output/Full/TAV_airflow' \
+full_path = '../csv_output/Full2014/TAV_airflow' \
       + '%s-%s'%(startF.strftime("%Y%m%d"), endF.strftime("%Y%m%d"))\
       + '.csv'
 df_full.to_csv(full_path)
 df_full_aug = tav_graphs_functions.data_ratios(df_full, points) 
-full_ratio_path = '../csv_output/Full/TAV_ratios' \
+full_ratio_path = '../csv_output/Full2014/TAV_ratios' \
       + '%s-%s'%(startF.strftime("%Y%m%d"), endF.strftime("%Y%m%d"))\
       + '.csv'
 df_full_aug.to_csv(full_ratio_path)

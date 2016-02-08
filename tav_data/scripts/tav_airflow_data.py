7b880167b228bc2f66891a2e5623eb88c978b7c7
soazig <soazig.kaam@berkeley.edu>
2016-02-08 00:23:42 -0800

    Script ready to dwn data for temperature and tav

--- tav_data/scripts/tav_airflow_data.py
+++ tav_data/scripts/tav_airflow_data.py
@@ -17,7 +17,7 @@ import datetime
 sys.path.append(os.path.join(os.path.dirname(__file__), "../functions/"))
 import tav_graphs_functions
 
-def tav_data(c, source, points, floors, not_zones, floor_frames, startF, endF, delta, window, source1=None):
+def tav_data(c, source, restrict_root, points, floors, not_zones, floor_frames, startF, endF, delta, window, ts_step, path_and=None):
   for f in floors:
     zones_names = ['S%s-%s' %(f, str(z).zfill(2)) for z in range(1,22)]
     #zones_names = ['S%s-%s' %(f, str(z).zfill(2)) for z in range(1,3)]
@@ -29,57 +29,64 @@ def tav_data(c, source, points, floors, not_zones, floor_frames, startF, endF, d
       print "\nStarted process for zone: ", zone_name
       zone_frames = []
       start = startF
-      end = start + delta
+      end = start + min(delta, (endF - startF))
       while start != end: 
         startDate = start.strftime("%m/%d/%Y %H:%M")
         endDate = end.strftime("%m/%d/%Y %H:%M")
         print "\nStart date: ", startDate
         print "End date: ", endDate
-        restrict = tav_graphs_functions.restrict(source, [zone_name], points, path_and=source1) 
+        restrict = tav_graphs_functions.restrict_zones(restrict_root, [zone_name], path_and) 
         query_data = tav_graphs_functions.query_data(c, restrict, startDate, endDate, win=window)
         #pdb.set_trace()
         tags = query_data[0]
         data = query_data[1]
+        dff = tav_graphs_functions.dff_ts(start, end, ts_step)
+        df = tav_graphs_functions.data_frame(dff, data, tags, points, tag_mode='Path') #, zones = True, headers=int(1))
         #pdb.set_trace()
-        df = tav_graphs_functions.data_frame(data, tags, points, mode='Path')
         zone_frames.append(df)
         start = end
         end += min(delta, (endF - end))
-      #pdb.set_trace()
       df_zone = pd.concat(zone_frames)
       floor_frames.append(df_zone)
     #pdb.set_trace()
     df_floor = reduce(lambda x,y: \
                pd.merge(x, y, on=['timestamp','datetime']),floor_frames)
-    #floor_path = '../csv_output/TEST/' + 'Floor%s_airflow' %(str(f).zfill(2))  \
-    floor_path = '../csv_output/Floor%s_3min/'%(startF.year) + 'Floor%s_tav' %(str(f).zfill(2))  \
+#    floor_path = '../csv_output/TEST/' + 'Floor%s_airflow' %(str(f).zfill(2))  \
+    floor_path = '../csv_output/Floor%s_3min/'%(startF.year) + 'Floor%s_temp' %(str(f).zfill(2))  \
         + '%s-%s'%(startF.strftime("%Y%m%d"), endF.strftime("%Y%m%d"))\
         + '.csv' 
     df_floor.to_csv(floor_path)
 #    df_aug = tav_graphs_functions.data_ratios(df_floor, points) 
-#    #floor_ratio_path = '../csv_output/TEST/' + 'Floor%s_ratios' %(str(f).zfill(2))  \
 #    floor_ratio_path = '../csv_output/Floor%s/'%(startF.year) + 'Floor%s_ratios' %(str(f).zfill(2))  \
+#    floor_ratio_path = '../csv_output/TEST/' + 'Floor%s_ratios' %(str(f).zfill(2))  \
 #        + '%s-%s'%(startF.strftime("%Y%m%d"), endF.strftime("%Y%m%d"))\
 #        + '.csv' 
 #    df_aug.to_csv(floor_ratio_path)
+#    pdb.set_trace()
 
 c = SmapClient(base='http://new.openbms.org/backend',\
                  key=['XuETaff882hB6li0dP3XWdiGYJ9SSsFGj0N8'])
-#source = 'Sutardja Dai Hall BACnet'
-source = 'Sutardja Dai Hall TAV'
-#source1 = None
-source1 = 'tav_whole_bldg' 
+source = 'Sutardja Dai Hall BACnet'
+source_tav = 'Sutardja Dai Hall TAV'
+source_energy = 'Sutardja Dai Hall Energy Data'
+path_and_tav = ['tav_whole_bldg/']
 #points = ['CTL_FLOW_MAX', 'CTL_FLOW_MIN', 'AIR_VOLUME'] 
-points = ['average_airflow_in_cycle', 'average_airflow_in_hour', 'tav_active'] 
+points = ['ROOM_TEMP', 'CTL_STPT']
+points_tav = ['average_airflow_in_cycle', 'average_airflow_in_hour', 'tav_active'] 
+startF = date(2015,10, 15)
+endF = date(2015, 12, 26)
+delta = datetime.timedelta(days=5) #try with 3 days - 1min resolution next time
+ts_step = 3*60 #timestamp fq
+window='apply window(first, field=\"minute\", width=3) to' 
+restrict = tav_graphs_functions.restrict(source, points) 
+restrict_tav = tav_graphs_functions.restrict(source_tav, points_tav, path_and_tav)
+restrict_root = '( ' + restrict + ') or (' + restrict_tav + ')'
+
 floors = [str(f) for f in [4,5,6,7,3,2,1]]
 #floors = [str(f) for f in [4, 5]]
 not_zones = ['S1-11', 'S1-12', 'S1-21', 'S6-21', 'S7-17', 'S7-18','S7-19', 'S7-20', 'S7-21']
 floor_frames = []
-startF = date(2015,11,9)
-endF = date(2015, 12, 26)
-delta = datetime.timedelta(days=3) #try with 3 days - 1min resolution next time
-window='apply window(first, field=\"minute\", width=3) to' 
 
-tav_data(c, source, points, floors, not_zones, floor_frames, startF, endF, delta, window, source1=source1)
+tav_data(c, source, restrict_root, points + points_tav, floors, not_zones, floor_frames, startF, endF, delta, window, ts_step, path_and=None)
 pdb.set_trace()
 

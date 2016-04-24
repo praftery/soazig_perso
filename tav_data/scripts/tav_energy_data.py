c29f4a96db06da1cd043ae2139df1d94732a45e6
soazig <soazig.kaam@berkeley.edu>
2016-04-24 02:18:20 -0700

    script to deal with query higher level (no zone)

--- tav_data/scripts/tav_energy_data.py
+++ tav_data/scripts/tav_energy_data.py
@@ -1,4 +1,6 @@
 """
+Use this script to dwnl data at a higher level - not at the zone level.
+Consider cost and energy seperately.
 @author Soazig Kaam <soazig.kaam@berkeley.edu>
 
 """
@@ -16,12 +18,11 @@ import datetime
 sys.path.append(os.path.join(os.path.dirname(__file__), "../functions/"))
 import tav_graphs_functions
 
-def energy_data(path_list, restrict, startF, endF, delta, ts, window):
+def energy_data(path_list, restrict, startF, endF, delta, ts, window, folder_name, file_name):
   start = startF
   end = start + min(delta, (endF - start))
   time_frames = []
   while start != end:
-    #pdb.set_trace()
     startDate = start.strftime("%m/%d/%Y %H:%M")
     endDate = end.strftime("%m/%d/%Y %H:%M")
     print "Start date: ", startDate
@@ -33,20 +34,21 @@ def energy_data(path_list, restrict, startF, endF, delta, ts, window):
     query_data = tav_graphs_functions.query_data(c, restrict, startDate, endDate, win=window)
     data = query_data[1]
     tags = query_data[0]
-    dt_format = '%Y-%m-%d %H:%M:%S'
     dff = tav_graphs_functions.dff_ts(start, end, ts)
+    #pdb.set_trace()
+    #dt_format = '%Y-%m-%d %H:%M:%S'
     #dff = pd.DataFrame()
     #dff['datetime'] = ts
     #dff['timestamp'] = pd.DatetimeIndex(dff['datetime']).astype(np.int64)/1000
     #[datetime.datetime.fromtimestamp(x/1000).strftime(dt_format)
     #                   for x in dff['timestamp']]
     df = tav_graphs_functions.data_frame(dff, data, tags, path_list, tag_mode='Path')
-    time_frames.append(df)
+    time_frames.append(df) 
     start = end
     end += min(delta, (endF - end))
-  df_period = reduce(lambda x,y:\
-              pd.merge(x, y, on=['timestamp','datetime']), time_frames)
-  df_period.to_csv('../csv_output/Energy%s/'%(startF.year) + 'Total_load%s-%s' \
+  df_full_period = pd.concat(time_frames)
+  df_full_period.to_csv('../csv_output_V2/' + folder_name + '%s/'%(startF.year) \
+    + file_name + '%s-%s' \
     %(startF.strftime("%Y%m%d"), endF.strftime("%Y%m%d")) \
     + '.csv')
 
@@ -55,59 +57,71 @@ c = SmapClient(base='http://new.openbms.org/backend',\
 source_energy = 'Sutardja Dai Hall Energy Data'
 path_and_energy = 'energy_data/variable_elec_cost/'
 path_list_energy = [
-               # 'total_cost',
-               # 'AH2_panel_fan_power',
-               # 'AH2_panel_fan_power_cost',
-               # 'chilled_water_AH2',
-               # 'chilled_water_AH2_cost',
-               # 'hot_water_AH2',
-               # 'hot_water_AH2_cost'
-                'total_zone_load_AH2'
-                ]
-path_not_energy = ['chilled_water_AH2A', 'chilled_water_AH2B']
-#path_list_energy = [
-#                'total_zone_load_AH2',
-#                'instantaneous_zone_load_S4-03'
-#                'instantaneous_zone_load_S4-05'
-#                'instantaneous_zone_load_S4-02'
-#                'instantaneous_zone_load_S4-18'
-#                'hot_water_S4-03'
-#                'hot_water_S4-05'
-#                'hot_water_S4-02'
-#                'hot_water_S4-18'
-#                ]
-path_list_supply = [
-                   'AH2_panel_fan_power',
-                   'AH2_panel_fan_power_cost',
-                   'AH2_total_supply_fan_power',
-                   'AH2_total_supply_fan_power_cost']
+                    'total_cost',
+                    'AH2_panel_fan_power',
+                    #'AH2_panel_fan_power_cost',
+                    'AH2_total_supply_fan_power',
+                    #'AH2_total_supply_fan_power_cost',
+                    'chilled_water_AH2',
+                    #'chilled_water_AH2_cost',
+                    'hot_water_AH2',
+                    #'hot_water_AH2_cost'
+                    'total_zone_load_AH2'
+                    'measured_chilled_water'
+                   ]
+path_not_energy = [
+                   'chilled_water_AH2A', 
+                   'chilled_water_AH2B'
+                  ]
+
 source_airflow = 'Sutardja Dai Hall BACnet'
 path_list_airflow = ['AH2A/SF_CFM', 'AH2B/SF_CFM']
 
-path_list = path_list_supply + path_list_airflow
+source_tav = 'Sutardja Dai Hall TAV'
+path_and_tav = 'tav_whole_bldg/'
+path_list_tav = [
+                 'zones_active',
+                 'zones_inactive',
+                 'zones_in_low_cycle',
+                 'zones_in_high_cycle',
+                 'zones_waiting_for_low_cycle',
+                 'zone_limit',
+                 'zones_entering_low_cycle',
+                 'zones_entering_high_cycle',
+                 'zones_being_activated',
+                 'zones_being_deactivated',
+                ]
+restrict_tav = " Metadata/SourceName = '%s' and Path ~ '%s' and ("\
+             %(source_tav, path_and_tav)\
+             + ' or '.join(["Path ~ '%s'"] * len(path_list_tav)) \
+             %tuple(path_list_tav) + ")" 
 
 restrict_energy = " Metadata/SourceName = '%s' and Path ~ '%s' and ("\
              %(source_energy, path_and_energy) \
              + ' or '.join(["Path ~ '%s'"] * len(path_list_energy)) \
-             %tuple(path_list_energy) + ") and not ("\
+             %tuple(path_list_energy) + ")  and not ("\
              + ' or '.join(["Path ~ '%s'"] * len(path_not_energy)) \
              %tuple(path_not_energy) + ")"
-restrict_supply = " Metadata/SourceName = '%s' and Path ~ '%s' and ("\
-             %(source_energy, path_and_energy) \
-             + ' or '.join(["Path ~ '%s'"] * len(path_list_supply)) \
-             %tuple(path_list_supply) + ")" 
+
 restrict_airflow = " Metadata/SourceName = '%s' and ("\
              %(source_airflow)\
              + ' or '.join(["Path ~ '%s'"] * len(path_list_airflow)) \
              %tuple(path_list_airflow) + ")" 
-#restrict = "(" + restrict_supply + ") or (" + restrict_airflow + ")"                  
-restrict = restrict_energy
-path_list = path_list_energy
+#folder_name='Energy'
+#file_name='Energy_TAV'
+#restrict = "(" + restrict_energy + ") or (" + restrict_airflow + ")"                  
+#path_list = path_list_energy + path_list_airflow
+
+folder_name='TAV'
+file_name='TAV_trends'
+restrict = restrict_tav
+path_list = path_list_tav
+
 #pdb.set_trace()
-startF = date(2014, 10, 15)
-endF = date(2014, 12, 26)
+startF = datetime.datetime(2016, 04, 01, 0, 0, 0)
+endF = datetime.datetime(2016, 04, 23, 0, 0, 0)
 delta = datetime.timedelta(days=80)
-ts = 3*60
-window = 'apply window(mean, field=\"minute\", width=3) to'
-energy_data(path_list, restrict, startF, endF, delta, ts, window)
+ts = 3*60 #window * 60s
+window = 'apply window(first, field=\"minute\", width=3) to'
+energy_data(path_list, restrict, startF, endF, delta, ts, window, folder_name, file_name)
 pdb.set_trace()
